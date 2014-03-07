f1d0ec1e00895d96f144b66af53b9f8e63e8b74d
Fikirte Derso <ftekderso@gmail.com>
2014-03-07 00:59:15 -0500

    Initial Commit

---
+++ RockPaperScisseApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RockPaperScisseApp
+//
+//  Created by Fikirte Derso on 3/7/14.
+//  Copyright (c) 2014 Fikirte Derso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RockPaperScisseApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RockPaperScisseApp
+//
+//  Created by Fikirte Derso on 3/7/14.
+//  Copyright (c) 2014 Fikirte Derso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
